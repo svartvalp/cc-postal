44523cf741a85d1aff5a34fbc6dc129bdbf44c34
Козлов Павел Вячеславович <pkozlov@phoenixit.ru>
2020-07-31 19:59:53 +0300

    Added navigation bar and login page

---
+++ client/src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from '@/App'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: App
+        },
+        {
+            path: '/login',
+            component: () => import('./components/Login')
+        },
+        {
+            path: '/main',
+            component: () => import('./views/Main')
+        }
+    ]
+})
---
+++ client/src/views/Main.vue
@@ -0,0 +1,93 @@
+<template>
+    <v-app id="inspire">
+        <v-navigation-drawer
+                v-model="drawer"
+                app
+                dark
+        >
+            <v-list dense>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-home</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-email</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Departures</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-account</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Account</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-arrow-left-circle</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Log out</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar
+                app
+                color="amber lighten-3"
+                dark
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" color="black"></v-app-bar-nav-icon>
+            <v-toolbar-title class="black--text">cc-postal</v-toolbar-title>
+
+        </v-app-bar>
+
+        <v-main>
+            <v-container
+                    class="fill-height"
+                    fluid
+            >
+                <v-row
+                        align="center"
+                        justify="center"
+                >
+
+                </v-row>
+            </v-container>
+        </v-main>
+        <v-footer
+                color="amber lighten-3"
+                app
+        >
+            <v-spacer></v-spacer>
+            <span class="black--text">&copy; {{ new Date().getFullYear() }}</span>
+        </v-footer>
+        <router-view />
+    </v-app>
+</template>
+
+<script>
+
+    export default {
+        name: 'App',
+
+        components: {
+        },
+        props: {
+            source: String,
+        },
+
+        data: () => ({
+            drawer: null,
+        }),
+    };
+</script>
