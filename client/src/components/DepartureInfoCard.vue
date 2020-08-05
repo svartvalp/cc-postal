a4c796c5dcc7e47f997f1826f17a0bf63140e27b
mtugarev <mtugarev@phoenixit.ru>
2020-08-05 18:25:35 +0300

    Added all departures and specific departure screens

---
+++ client/src/components/DepartureInfoCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-col cols="12">
+    <v-card class="mx-auto" max-width="600">
+      <v-alert outlined color="black">
+        <v-card-title>
+          <label for="param">{{ paramName }}</label>
+        </v-card-title>
+        <v-card-text id="param">{{ paramValue }}</v-card-text>
+      </v-alert>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    paramName: {
+      type: String,
+      required: true
+    },
+    paramValue: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+<style>
+</style>
