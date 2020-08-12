3e052afb77d96060dae33f794c3e2faaa0e4d5fb
Касьяненко Константин Владимирович <kkasyanenko@phoenixit.ru>
2020-08-12 12:14:21 +0300

    Updated changing drawerState without mutating prop, updating user in localStorage

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,91 +1,91 @@
 <template>
-    <div id="app">
+  <div id="app">
 
-        <v-app id="inspire">
-            <Header
-                    :drawerState="drawerState"
-                    :isAuthenticated="isAuthenticated"
-                    @drawBar="drawBar"
-            />
-            <NavigationBar
-                    :drawerState="drawerState"
-                    @logout="logout"
-                    v-if="isAuthenticated"
-            />
-            <MainPanel
-                    v-bind:user="user"
-                    @login="login"
-                    @logout="logout"
-                    @update-user="updateUser"
-            />
-            <Footer/>
-        </v-app>
-    </div>
+    <v-app id="inspire">
+      <Header
+          :isAuthenticated="isAuthenticated"
+          @drawBar="drawBar"
+      />
+      <NavigationBar
+          :drawerState="drawerState"
+          @logout="logout"
+          v-if="isAuthenticated"
+      />
+      <MainPanel
+          v-bind:user="user"
+          @login="login"
+          @logout="logout"
+          @update-user="updateUser"
+      />
+      <Footer/>
+    </v-app>
+  </div>
 </template>
 
 <script>
-    import Header from '@/components/root/Header'
-    import NavigationBar from '@/components/root/NavigationBar'
-    import Footer from '@/components/root/Footer'
-    import MainPanel from '@/components/root/MainPanel'
+import Header from '@/components/root/Header'
+import NavigationBar from '@/components/root/NavigationBar'
+import Footer from '@/components/root/Footer'
+import MainPanel from '@/components/root/MainPanel'
 
-    export default {
-        name: 'App',
+export default {
+  name: 'App',
 
-        components: {
-            Header,
-            NavigationBar,
-            MainPanel,
-            Footer
-        },
-        props: {
-            source: String,
-        },
-        data() {
-            return {
-                jwt: localStorage.getItem('jwt'),
-                drawerState: false,
-                isAuthenticated: false,
-                user: {}
-            }
-        },
-        methods: {
-            drawBar(drawerState) {
-                this.drawerState = !drawerState;
-            },
-            logout() {
-                localStorage.clear();
-                this.$http.defaults.headers.common['Authorization'] = '';
-                this.$router.push('/login')
-                this.isAuthenticated = false
-                this.drawerState = false
-            },
-            login(user) {
-                this.isAuthenticated = true
-                this.user = user
-                localStorage.setItem('user', JSON.stringify(this.user))
-            },
-            updateUser(user){
-              this.user= user
-            }
-        },
-        created() {
-            let token = localStorage.getItem('jwt')
-            if (token != null) {
-                this.$http.defaults.headers.common['Authorization'] = token
-                this.$http
-                    .get(`/user`)
-                    .then((response) => {
-                        this.isAuthenticated = true
-                        this.user = response.data
-                        localStorage.setItem('user', JSON.stringify(this.user))
-                    })
-                    .catch(() => {
-                        this.$http.defaults.headers.common['Authorization'] = ''
-                        this.logout()
-                    })
-            }
-        }
-    };
+  components: {
+    Header,
+    NavigationBar,
+    MainPanel,
+    Footer
+  },
+  props: {
+    source: String,
+  },
+  data() {
+    return {
+      jwt: localStorage.getItem('jwt'),
+      drawerState: false,
+      isAuthenticated: false,
+      user: {}
+    }
+  },
+  methods: {
+    drawBar() {
+      this.drawerState = !this.drawerState;
+    },
+    logout() {
+      localStorage.clear();
+      this.$http.defaults.headers.common['Authorization'] = '';
+      this.$router.push('/login')
+      this.isAuthenticated = false
+      this.drawerState = false
+    },
+    login(user) {
+      this.isAuthenticated = true
+      this.user = user
+      localStorage.setItem('user', JSON.stringify(this.user))
+    },
+    updateUser(user) {
+      this.user = user
+      localStorage.setItem('user', JSON.stringify(this.user))
+    }
+  },
+  created() {
+    let token = localStorage.getItem('jwt')
+    if (token != null) {
+      this.$http.defaults.headers.common['Authorization'] = token
+      this.$http
+          .get(`/user`)
+          .then((response) => {
+            this.isAuthenticated = true
+            this.user = response.data
+            localStorage.setItem('user', JSON.stringify(this.user))
+          })
+          .catch(() => {
+            this.$http.defaults.headers.common['Authorization'] = ''
+            this.logout()
+          })
+    }
+  }
+};
 </script>
 
--- client/src/components/root/Header.vue
+++ client/src/components/root/Header.vue
@@ -1,32 +1,32 @@
 <template>
-    <v-app-bar
-            app
-            color="amber lighten-3"
-            dark
-    >
-        <v-app-bar-nav-icon color="black" v-if="isAuthenticated" v-on:click=clickHandling></v-app-bar-nav-icon>
-        <v-toolbar-title class="black--text">ПочтаЛён</v-toolbar-title>
+  <v-app-bar
+      app
+      color="amber lighten-3"
+      dark
+  >
+    <v-app-bar-nav-icon color="black" v-if="isAuthenticated" v-on:click=clickHandling></v-app-bar-nav-icon>
+    <v-toolbar-title class="black--text">ПочтаЛён</v-toolbar-title>
 
-    </v-app-bar>
+  </v-app-bar>
 </template>
 
 <script>
-    export default {
-        props: ['drawerState', 'isAuthenticated'],
-        methods: {
-            clickHandling() {
-                this.$emit('drawBar', this.drawerState)
-            }
-        },
-        computed: {
-            hasToken() {
-                return localStorage.getItem('jwt') !== null
-            },
-        }
+export default {
+  props: ['isAuthenticated'],
+  methods: {
+    clickHandling() {
+      this.$emit('drawBar')
     }
+  },
+  computed: {
+    hasToken() {
+      return localStorage.getItem('jwt') !== null
+    },
+  }
+}
 </script>
 <style>
-    .black--text {
-        font-size: 16pt;
-    }
+.black--text {
+  font-size: 16pt;
+}
 </style>
